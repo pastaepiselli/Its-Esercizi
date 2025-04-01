6ccdfb8a10f2284fc31b32b18cad4564826bf11a
pastaepiselli <rossilorenzo371@gmail.com>
2025-04-01 09:57:06 +0200

    aggiornamentii

--- Lezione_5/esercizio_lepre_tartaruga/movimenti.py
+++ Lezione_5/esercizio_lepre_tartaruga/movimenti.py
@@ -1,186 +1,262 @@
 import random
 def movetart(tart_pos):
 
-    # genero numero casuale da 1 a 10
+    #genero numero casuale da 1 a 10
 
     movement = random.randint(1, 10)
+    
+    #stamina
+
+    stamina_tart = 100
 
     match movement:
         
         #passo veloce
         case 1 | 2 | 3 | 4 | 5:
-            # ogni volta che aggiungo dei movimenti controllo se siano 70 o se lo superino cosi da evitare indexing error
-            if tart_pos + 3 >= 70:
-                # quando supeerano o sono uguali ritorno 70 
-                return 70
+            if stamina_tart >= 5:
+                stamina_tart -= 5
+                # ogni volta che aggiungo dei movimenti controllo se siano 70 o se lo superino cosi da evitare indexing error
+                if tart_pos + 3 >= 70:
+                    # quando supeerano o sono uguali ritorno 70 
+                    return 70
+                else:
+                    tart_pos += 3
             else:
-                tart_pos += 3
+                stamina_tart += 10
 
         
         #scivolata
         case 6 | 7:
-            # controllo se puo tornare indietro oppure sfora
-            if tart_pos > 6:
+            if stamina_tart >= 10:
+                stamina_tart -= 10
+                # controllo se puo tornare indietro oppure sfora
+                if tart_pos > 6:
                
-                tart_pos -= 6
+                    tart_pos -= 6
+                else:
+        
+                    tart_pos = 0
             else:
-        
-                tart_pos = 0
-        
+
+                stamina_tart += 10
         #passo lento
         case 8 | 9 | 10:
+            if stamina_tart >= 3:
+                stamina_tart -= 3
 
-            if tart_pos + 1 >= 70:
+                if tart_pos + 1 >= 70:
 
-                return 70
+                    return 70
+                else:
+                    tart_pos += 1
             else:
-                tart_pos += 1
+                stamina_tart += 10
 
     return tart_pos
 
 def movelepre(lepre_pos):
     # qua la shit e molto semplice
     movement = random.randint(1, 10)
-
+    stamina_lepre = 100
     match movement:
         
         # riposo
         case 1 | 2:
 
-            lepre_pos += 0 
+            lepre_pos += 0
+            #controllo che la stamina non superi mai 100
+            if stamina_lepre + 10 > 100:
+
+                stamina_lepre = 100
+            else:
+                stamina_lepre += 10
 
         # grande balzo
         case 3 | 4:
+            if stamina_lepre >= 15:
 
-            if lepre_pos + 9 >= 70:
-                return 70
-            else:
-                lepre_pos += 9
+                stamina_lepre -= 15
+
+                if lepre_pos + 9 >= 70:
+                    return 70
+                else:
+                    lepre_pos += 9
+            
 
         
         # grande scivolata
         case 5:
+            if stamina_lepre >= 20:
 
-            lepre_pos -= 12
+                stamina_lepre -= 20
 
-            if lepre_pos <= 12:
+                lepre_pos -= 12
 
-                lepre_pos = 0
+                if lepre_pos <= 12:
+
+                    lepre_pos = 0
 
 
         
         # piccolo balzo
         case 6 | 7 | 8:
-            
-            if lepre_pos + 1 >= 70:
+            if stamina_lepre >= 5:
 
-                return 70
-            else:
-                lepre_pos += 1
+                stamina_lepre -= 5
+            
+                if lepre_pos + 1 >= 70:
+
+                    return 70
+                else:
+                    lepre_pos += 1
         
         # piccola scivolata   
         case 9 | 10:
+           
+           if stamina_lepre >= 8:
+                
+                stamina_lepre -= 8
 
-           lepre_pos -= 2
-           if lepre_pos <= 2:
+                lepre_pos -= 2
+                if lepre_pos <= 2:
             
-            lepre_pos = 0
+                   lepre_pos = 0
 
         
     return lepre_pos
 
 def movetart_rain(tart_pos):
 
-    # genero numero casuale da 1 a 10
+    #genero numero casuale da 1 a 10
 
     movement = random.randint(1, 10)
+    
+    #stamina
+
+    stamina_tart = 100
 
     match movement:
         
         #passo veloce
         case 1 | 2 | 3 | 4 | 5:
-            # ogni volta che aggiungo dei movimenti controllo se siano 70 o se lo superino cosi da evitare indexing error
-            if tart_pos + 3 >= 70:
-                # quando supeerano o sono uguali ritorno 70 
-                return 70
+            if stamina_tart >= 5:
+                stamina_tart -= 5
+                # ogni volta che aggiungo dei movimenti controllo se siano 70 o se lo superino cosi da evitare indexing error
+                if tart_pos + 3 >= 70:
+                    # quando supeerano o sono uguali ritorno 70 
+                    return 70
+                else:
+                    tart_pos += 3
             else:
-                tart_pos += 3
+                stamina_tart += 10
 
         
         #scivolata
         case 6 | 7:
-            # controllo se puo tornare indietro oppure sfora
-            if tart_pos > 6:
+            if stamina_tart >= 10:
+                stamina_tart -= 10
+                # controllo se puo tornare indietro oppure sfora
+                if tart_pos > 6:
                
-                tart_pos -= 6
+                    tart_pos -= 6
+                else:
+        
+                    tart_pos = 0
             else:
-        
-                tart_pos = 0
-        
+
+                stamina_tart += 10
         #passo lento
         case 8 | 9 | 10:
+            if stamina_tart >= 3:
+                stamina_tart -= 3
 
-            if tart_pos + 1 >= 70:
+                if tart_pos + 1 >= 70:
 
-                return 70
+                    return 70
+                else:
+                    tart_pos += 1
             else:
-                tart_pos += 1
+                stamina_tart += 10
 
-    if tart_pos - 2 <= 0:
+    if tart_pos - 1 <  0:
+
         return 0
-    else: 
-        return tart_pos
+    else:
+
+        return tart_pos - 1
 
 def movelepre_rain(lepre_pos):
     # qua la shit e molto semplice
     movement = random.randint(1, 10)
-
+    stamina_lepre = 100
     match movement:
         
         # riposo
         case 1 | 2:
 
-            lepre_pos += 0 
+            lepre_pos += 0
+            #controllo che la stamina non superi mai 100
+            if stamina_lepre + 10 > 100:
+
+                stamina_lepre = 100
+            else:
+                stamina_lepre += 10
 
         # grande balzo
         case 3 | 4:
+            if stamina_lepre >= 15:
 
-            if lepre_pos + 9 >= 70:
-                return 70
-            else:
-                lepre_pos += 9
+                stamina_lepre -= 15
+
+                if lepre_pos + 9 >= 70:
+                    return 70
+                else:
+                    lepre_pos += 9
+            
 
         
         # grande scivolata
         case 5:
+            if stamina_lepre >= 20:
 
-            lepre_pos -= 12
+                stamina_lepre -= 20
 
-            if lepre_pos <= 12:
+                lepre_pos -= 12
 
-                lepre_pos = 0
+                if lepre_pos <= 12:
+
+                    lepre_pos = 0
 
 
         
         # piccolo balzo
         case 6 | 7 | 8:
-            
-            if lepre_pos + 1 >= 70:
+            if stamina_lepre >= 5:
 
-                return 70
-            else:
-                lepre_pos += 1
+                stamina_lepre -= 5
+            
+                if lepre_pos + 1 >= 70:
+
+                    return 70
+                else:
+                    lepre_pos += 1
         
         # piccola scivolata   
         case 9 | 10:
+           
+           if stamina_lepre >= 8:
+                
+                stamina_lepre -= 8
 
-           lepre_pos -= 2
-           if lepre_pos <= 2:
+                lepre_pos -= 2
+                if lepre_pos <= 2:
             
-            lepre_pos = 0
+                   lepre_pos = 0
 
         
-    if lepre_pos - 2 <= 0:
+    if lepre_pos - 2 < 0:
+
         return 0
-    else: 
-        return lepre_pos
+    else:
+
+        return lepre_pos - 2
--- Lezione_5/gioco_tartaruga/tartarugagame.py
+++ Lezione_5/gioco_tartaruga/tartarugagame.py
@@ -3,23 +3,3 @@ import pygame
 
 width: int = 640
 height: int = 420
-
-BACKGROUND = pygame.image.load('/home/lorenzo-rossi/Its-Esercizi/Lezione_5/gioco_tartaruga/assets/backgroundtart.jpg')
-
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('tartarga e la lepre')
-
-clock = pygame.time.Clock()
-running: bool = True
-
-while running:
-    
-    screen.blit(BACKGROUND, (0, 0))
-    for event in pygame.event.get():
-
-        if event.type == pygame.QUIT:
-
-            running = False
-    pygame.display.update()
-    clock.tick(60)
-pygame.quit()
